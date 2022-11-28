acb9254ad2cccd0aad84f4c63957be4b9a91ff23
rbk6 <rmbackman@gmail.com>
2022-11-27 18:41:33 -0800

    created doctor portal

---
+++ doctor/index.js
@@ -0,0 +1,39 @@
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchBar");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("patientSearch");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function getDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = mm + '-' + dd + '-' + yyyy;
+  
+  document.getElementById('date').value = today;
+}
+
+window.onload = function() {
+  getDate();
+};
+
+var today = new Date().toISOString().slice(0, 16);
+document.getElementsByName("book")[0].min = today;
